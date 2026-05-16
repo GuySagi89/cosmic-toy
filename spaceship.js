8700544aaa97021edfc8140042d98a723be4efc2
GuySagi89 <Guysimso@gmail.com>
2026-05-16 19:12:58 +0300

    Update spaceship.js

--- spaceship.js
+++ spaceship.js
@@ -345,8 +345,10 @@
       const aimDx = spaceship._aimX - spaceship.x;
       const aimDy = spaceship._aimY - spaceship.y;
       if (Math.hypot(aimDx, aimDy) > 5) {
-        spaceship.angle = Math.atan2(aimDy, aimDx) + Math.PI / 2;
+        spaceship.angle = lerpAngle(spaceship.angle, Math.atan2(aimDy, aimDx) + Math.PI / 2, dt * 12);
       }
+    } else if (spd > 25) {
+      spaceship.angle = lerpAngle(spaceship.angle, Math.atan2(spaceship.vy, spaceship.vx) + Math.PI / 2, dt * 8);
     }
 
     if (spd > 25 && window.smokeParticles.length < 300) {
